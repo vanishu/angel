039c507482373acd1714c6b0019748ec9fed0263
Angelo Prado <angelpm@gmail.com>
2014-08-26 01:19:06 -0700

    Initial checkin

---
+++ NameUniquenessCheck/NameUniquenessCheck/Program.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net;
+
+namespace NameUniquenessCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fullContactAPI = "https://api.fullcontact.com/v2/name/stats.json?name={0}&apiKey=76ae3732151391f3";
+            const string whitePagesAPI = "http://names.whitepages.com/{0}/{1}";
+
+            Console.WriteLine("Full Name? ");
+            String name = Console.ReadLine();
+            String firstName = null, lastName = null;
+
+            try
+            {
+                if (name.Contains(" "))
+                {
+                    var tokens = name.Split(new char[] { ' ' });
+                    firstName = tokens[0];
+                    lastName = tokens[tokens.Length - 1];
+                    using (WebClient wc = new WebClient())
+                    {
+                        String response = wc.DownloadString(String.Format(whitePagesAPI, WebUtility.UrlEncode(firstName), WebUtility.UrlEncode(lastName)));
+                        int target = response.IndexOf("head_count");
+                        String[] targetTokens = response.Substring(target, 100).Split(new char[] { '<', '>' });
+                        Console.WriteLine("\n-> " + targetTokens[3].Trim() + " people in the U.S. have this name");
+                    }
+                }
+
+                else
+                {
+                    using (WebClient wc = new WebClient())
+                    {
+                        String response = wc.DownloadString(String.Format(fullContactAPI, name.Trim()));
+                        JObject jobject = JObject.Parse(response);
+                        Console.WriteLine("Given Name - Rank: " + jobject["name"]["given"]["rank"] + " - Count: " + jobject["name"]["given"]["count"]);
+                        Console.WriteLine("Family Name - Rank: " + jobject["name"]["family"]["rank"] + " - Count: " + jobject["name"]["family"]["count"]);
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nERROR - Cannot determine uniqueness");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimonTwitter/Simon/Program.cs
@@ -0,0 +1,155 @@
+﻿using LinqToTwitter;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Simon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args)
+            {
+                Task runTask = RunRest(arg);
+                runTask.Wait();
+            }
+        }
+
+
+        private static async Task RunRest(string args)
+        {
+            string[] userSearchTerms = ConfigurationManager.AppSettings["searchValues"].Split(',');
+
+            var auth = new ApplicationOnlyAuthorizer
+            {
+                CredentialStore = new InMemoryCredentialStore
+                {
+                    ConsumerKey = ConfigurationManager.AppSettings["twitterConsumerKey"],
+                    ConsumerSecret = ConfigurationManager.AppSettings["twitterConsumerSecret"]
+                }
+            };
+
+            await auth.AuthorizeAsync();
+
+            var ctx = new TwitterContext(auth);
+
+            string[] searchTerms = args.Split(',');
+
+            for (int i = 0; i < searchTerms.Length; i++)
+                searchTerms[i] = searchTerms[i].Trim();
+
+            string query =
+                string.Join(" OR ",
+                    (from term in searchTerms
+                     select "\"" + term.Trim() + "\"")
+                    .ToArray());
+
+            var search =
+                await
+                (from srch in ctx.Search
+                 where srch.Type == SearchType.Search &&
+                       srch.Query == query
+                 select srch)
+                .SingleOrDefaultAsync();
+
+            var exactResults =
+                (from tweet in search.Statuses
+                 where searchTerms.Contains(tweet.Text.ToLower().Trim(), StringComparer.OrdinalIgnoreCase)
+                 select tweet)
+                .ToList();
+
+            exactResults.ForEach(tweet =>
+                {
+                    var now = DateTime.Now;
+                    var tweetedTime = tweet.CreatedAt.ToLocalTime();
+
+                    if ((now - tweetedTime).Minutes > Int32.Parse(ConfigurationManager.AppSettings["tweetedInTheLastXMinutes"])) return;
+
+                    string coordinates = "not available";
+                    if (tweet.Coordinates != null)
+                    {
+                        coordinates = tweet.Coordinates.Latitude + "," + tweet.Coordinates.Longitude;
+                    }
+
+                    string place = "not available";
+                    if (tweet.Place != null)
+                    {
+                        place = tweet.Place.FullName + "," + tweet.Place.Country + "," + tweet.Place.PlaceType;
+                    }
+
+                    Console.WriteLine(
+                        "User Name: {0}, \r\nScreen Name: {1}, \r\nTweet: {2}, \r\nProfile Image URL: {3}, \r\nCreated At: {4}, \r\n" +
+                        "Lat/Lon: {5}, \r\nBio: {6}, \r\nGeo Enabled: {7}, \r\nLocation: {8} \r\nPlace: {9}",
+                        tweet.User.Name,
+                        tweet.User.ScreenNameResponse,
+                        tweet.Text,
+                        tweet.User.ProfileImageUrl,
+                        tweet.CreatedAt.ToLocalTime(),
+                        coordinates,
+                        tweet.User.Description,
+                        tweet.User.GeoEnabled,
+                        tweet.User.Location,
+                        place);
+
+
+                    // Get tweets that the user cares about
+                    List<String> interestingTweets = new List<String>();
+                    var tweets = (from myTweet in ctx.Status
+                                  where myTweet.Type == StatusType.User &&
+                                        myTweet.ScreenName == tweet.User.ScreenNameResponse
+                                  select myTweet).ToList();
+
+                    foreach (var myTweet in tweets)
+                    {
+                        if (userSearchTerms.Any(myTweet.Text.ToLower().Trim().Contains))
+                        {
+                            Console.WriteLine("++ Found: " + myTweet.Text);
+                            interestingTweets.Add(myTweet.Text.Trim());
+                        }
+                    }
+
+                    Console.WriteLine("\r\n\r\n");
+
+
+                    // Write the HTML file
+                    String htmlOutputFile = Directory.GetParent(Assembly.GetExecutingAssembly().Location) + "\\tweetdata.html";
+
+                    if (!File.Exists(htmlOutputFile))
+                    {
+                        using (StreamWriter sw = new StreamWriter(htmlOutputFile, true))
+                        {
+                            sw.WriteLine(
+                            @"<input style='height:70px; font-size:40px' type='button' value='Reload' onClick=""window.location = '/svupload/uploads/tweetdata.html?t=' + Math.random();"">
+                              <br>" + DateTime.Now + "<br>");
+                        }
+                    }
+
+                    using (StreamWriter sw = new StreamWriter(htmlOutputFile, true))
+                    {
+                        sw.WriteLine(
+                        @"<h1>" + tweet.User.Name + @", @" + tweet.User.ScreenNameResponse + @"<br>
+                        <img src='" + tweet.User.ProfileImageUrl.Replace("_normal", "_400x400") + @"' width=150 height=150><br>
+                        </h1>
+                        <div style='font-size:20px;'>
+                        Tweet: <b>" + tweet.Text + @"</b><br>
+                        Location: <b>" + tweet.User.Location + @"</b><br>
+                        Bio: " + tweet.User.Description + @"<br>
+                        Place: " + tweet.User.Location + @"<br><br>");
+
+                        foreach (String str in interestingTweets)
+                        {
+                            sw.WriteLine("+<b><i>Tweet</i></b>: " + str + "<br>");
+                        }
+
+                        sw.WriteLine("</div>");
+                    }
+
+                });
+        }
+    }
+}
